8976f2faebe644fb488256bdfa35ac5fe4b267db
Zakaria El Jattari <zakaria.eljattari@student.ap.be>
2024-03-01 17:25:47 +0000

    bezig met het oplossen van bugs

--- console.ts
+++ console.ts
@@ -1,12 +1,54 @@
-import playerdata from "./players.json";
-import teamdata from "./teams.json";
-import interfacesdata from "./interfaces.ts";
-
-const player: Player[] = playerdata;
-const team: teams[] = teamdata;
-
-function showData(playerdata, teamdata,interfacesdata){
+import * as readline from 'readline-sync';
+import { players, teams } from "./interfaces.ts";
+import { exit } from 'process';
 
 
 
+console.log(" Welkom bij de JSON-gegevensviewer! ")
+console.log("1. Bekijk alle gegevens")
+console.log("2. Filter op ID")
+console.log("3. Sluit")
+
+
+
+let choice: number = Number(readline.question("Wat wil je doen?"))
+
+switch(choice){
+
+    case 1: showData(players,teams);
+        break;
+
+    case 2: filterByID(players);
+
+    case 3: exit;
+
+}
+
+function showData(players,teams){
+
+    for(let player of players){
+        console.log(player.name,player.id )
+    }
+
+}
+
+function filterByID(players) {
+    for(let player of players){
+        console.log(player.name )
+        console.log(player.id)
+        console.log(player.position)
+        console.log(player.age)
+        console.log(player.playing)
+        console.log(player.birthDate)
+        console.log(player.imageUrl)
+        console.log(player.rating)
+        console.log(player.trophies)
+        console.log(player.birthPlace)
+        console.log(player.team.id)
+        console.log(player.team.name)
+        console.log(player.team.Manager)
+        console.log(player.team.teamLogoUrl)
+        console.log(player.team.createDate)
+        console.log(player.team.league)
+    }
 }
--- interfaces.ts
+++ interfaces.ts
@@ -1,4 +1,12 @@
-interface Player {
+import playerdata from "./players.json";
+import teamdata from "./teams.json";
+
+
+export const players: Player[] = playerdata;
+export const teams: Team[] = teamdata;
+
+
+export interface Player {
             id: string,
             name: string,
             position: string,
@@ -9,11 +17,11 @@ interface Player {
             rating: number,
             trophies: string[],
             birthPlace: string ,
-            eam:
+            team: Team
             
 }
 
-interface team{
+export interface Team{
     
         id: string,
         name: string,
